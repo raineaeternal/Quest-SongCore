98db6d9d69a3de900620ce23b8521d465cb78407
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-03-06 23:46:22 +0100

    Minor improvements to tasks util

--- include/tasks.hpp
+++ include/tasks.hpp
@@ -2,6 +2,8 @@
 
 #include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
 #include "System/Threading/Tasks/Task_1.hpp"
+#include <chrono>
+#include <thread>
 
 namespace SongCore {
     template<typename Ret, typename T>
@@ -11,11 +13,17 @@ namespace SongCore {
     }
 
     template<typename Ret, typename T>
-    requires(std::is_invocable_r_v<Ret, T>)
+    requires(std::is_invocable_r_v<Ret, T, System::Threading::CancellationToken>)
     static void task_cancel_func(System::Threading::Tasks::Task_1<Ret>* task, T func, System::Threading::CancellationToken cancelToken) {
-        auto value = std::invoke(func);
+        using namespace std::chrono_literals;
+        // start func as an std::future
+        auto fut = il2cpp_utils::il2cpp_async(func, cancelToken);
+        // await the future
+        while (fut.wait_for(0ns) != std::future_status::ready && !cancelToken.IsCancellationRequested) std::this_thread::yield();
+
+        // if cancellation wasn't requested, set result, else set canceled
         if (!cancelToken.IsCancellationRequested) {
-            task->TrySetResult(std::invoke(func));
+            task->TrySetResult(fut.get());
         } else {
             task->TrySetCanceled(cancelToken);
         }
@@ -30,7 +38,7 @@ namespace SongCore {
     }
 
     template<typename Ret, typename T>
-    requires(!std::is_same_v<Ret, void> && std::is_invocable_r_v<Ret, T>)
+    requires(!std::is_same_v<Ret, void> && std::is_invocable_r_v<Ret, T, System::Threading::CancellationToken>)
     static System::Threading::Tasks::Task_1<Ret>* StartTask(T func, System::Threading::CancellationToken cancelToken) {
         auto t = System::Threading::Tasks::Task_1<Ret>::New_ctor();
         il2cpp_utils::il2cpp_aware_thread(&task_cancel_func<Ret, T>, t, func, cancelToken).detach();
