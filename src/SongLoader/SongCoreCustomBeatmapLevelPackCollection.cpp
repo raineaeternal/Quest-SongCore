62f38127089520b38cd84670bb6ea76f98185f4b
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-03-11 22:37:54 +0100

    Skip adding empty level packs

--- src/SongLoader/SongCoreCustomBeatmapLevelPackCollection.cpp
+++ src/SongLoader/SongCoreCustomBeatmapLevelPackCollection.cpp
@@ -1,6 +1,10 @@
 #include "SongLoader/SongCoreCustomBeatmapLevelPackCollection.hpp"
 #include "SongLoader/SongCoreCustomLevelPack.hpp"
 
+#include "GlobalNamespace/IBeatmapLevelCollection.hpp"
+#include "System/Collections/Generic/IReadOnlyList_1.hpp"
+#include "System/Collections/Generic/IReadOnlyCollection_1.hpp"
+
 DEFINE_TYPE(SongCore::SongLoader, SongCoreCustomBeatmapLevelPackCollection);
 
 namespace SongCore::SongLoader {
@@ -16,7 +20,10 @@ namespace SongCore::SongLoader {
         _beatmapLevelPacks = ArrayW<GlobalNamespace::IBeatmapLevelPack*>::Empty();
     }
 
-    void SongCoreCustomBeatmapLevelPackCollection::AddPack(SongCoreCustomLevelPack* levelPack) {
+    void SongCoreCustomBeatmapLevelPackCollection::AddPack(SongCoreCustomLevelPack* levelPack, bool addIfEmpty) {
+        // if we don't want to force the add, and the pack has 0 levels, don't add it
+        if (!addIfEmpty && levelPack->beatmapLevelCollection->beatmapLevels->i___System__Collections__Generic__IReadOnlyCollection_1_T_()->Count == 0) return;
+
         auto newArray = ArrayW<GlobalNamespace::IBeatmapLevelPack*>(_beatmapLevelPacks.size() + 1);
         std::copy_n(_beatmapLevelPacks->begin(), _beatmapLevelPacks.size(), newArray->begin());
         newArray[_beatmapLevelPacks.size()] = levelPack->i___GlobalNamespace__IBeatmapLevelPack();
