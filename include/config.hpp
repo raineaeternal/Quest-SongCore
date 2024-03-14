6d633e0494a7ae3200ce202e58ee12964ba0b732
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-03-14 16:02:44 +0100

    Add missing assets & add some config values which are yet to be properly applied

--- include/config.hpp
+++ include/config.hpp
@@ -3,6 +3,16 @@
 #include <filesystem>
 
 struct Config {
+    /// @brief whether to apply note colors from diffs
+    bool customSongNoteColors = true;
+    /// @brief whether to apply obstacle colors from diffs
+    bool customSongObstacleColors = true;
+    /// @brief whether to apply environment colors from diffs
+    bool customSongEnvironmentColors = true;
+
+    /// @brief whether the saber count override should be honored
+    bool disableOneSaberOverride = false;
+
     /// @brief path to the preferred folder where to store songs. This is exposed to people consuming the api as a getter
     std::filesystem::path PreferredCustomLevelPath = "/sdcard/ModData/com.beatgames.beatsaber/Mods/SongCore/CustomLevels";
     /// @brief multiple paths to folders to load songs from, in case user has multiple folders. Not exposed
