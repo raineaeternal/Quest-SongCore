e28105c38ec59bfc18bc0d7da989aac27fc29c0b
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-03-14 22:03:59 +0100

    Remove early exit

--- scripts/createqmod.ps1
+++ scripts/createqmod.ps1
@@ -17,9 +17,6 @@ if ($help -eq $true) {
 
 $mod = "./mod.json"
 
-if ($LASTEXITCODE -ne 0) {
-    exit $LASTEXITCODE
-}
 $modJson = Get-Content $mod -Raw | ConvertFrom-Json
 
 if ($qmodName -eq "") {
