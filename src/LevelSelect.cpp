670b93cd9843259072363313e298c38648cd117b
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-03-15 17:40:59 +0100

    Cleanup playbutton disabling using level select event

--- src/LevelSelect.cpp
+++ src/LevelSelect.cpp
@@ -2,8 +2,13 @@
 
 #include "CustomJSONData.hpp"
 #include "SongCore.hpp"
+
+#include "SongLoader/RuntimeSongLoader.hpp"
+
+#include "GlobalNamespace/IPreviewBeatmapLevel.hpp"
 #include "GlobalNamespace/IDifficultyBeatmap.hpp"
 #include "GlobalNamespace/IDifficultyBeatmapSet.hpp"
+#include "SongLoader/RuntimeSongLoader.hpp"
 #include "bsml/shared/Helpers/delegates.hpp"
 
 DEFINE_TYPE(SongCore, LevelSelect);
@@ -46,6 +51,7 @@ namespace SongCore {
             eventArgs.beatmapLevel = GetSelectedBeatmapLevel();
             eventArgs.difficultyBeatmapSet = GetSelectedDifficultyBeatmapSet();
             eventArgs.difficultyBeatmap = GetSelectedDifficultyBeatmap();
+            eventArgs.levelID = static_cast<std::string>(eventArgs.previewBeatmapLevel->levelID);
 
             auto customLevel = il2cpp_utils::try_cast<GlobalNamespace::CustomDifficultyBeatmap>(eventArgs.beatmapLevel).value_or(nullptr);
             if (customLevel) {
@@ -77,6 +83,14 @@ namespace SongCore {
 
     void LevelSelect::HandleCustomLevelWasSelected(LevelWasSelectedEventArgs& eventArgs) {
         if (!eventArgs.isCustom) return;
+
+        eventArgs.isWIP = eventArgs.levelID.ends_with(" WIP");
+        // get a view into the levelid
+        std::string_view hashView(eventArgs.levelID);
+        // remove `custom_level_` from the front
+        hashView = hashView.substr(SongLoader::RuntimeSongLoader::CUSTOM_LEVEL_PREFIX_ID.size());
+        // possibly remove ` WIP` from the end
+        eventArgs.hash = hashView.substr(0, hashView.size() - (eventArgs.isWIP ? 4 : 0));
 
         auto saveData = il2cpp_utils::try_cast<CustomJSONData::CustomLevelInfoSaveData>(eventArgs.customBeatmapLevel->standardLevelInfoSaveData).value_or(nullptr);
         if (!saveData) return;
