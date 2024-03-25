ae4fd832ba7475274b6046cac581285b4a7bd08e
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-03-25 22:00:05 +0100

    Change how updater works

--- include/SongLoader/NavigationControllerUpdater.hpp
+++ include/SongLoader/NavigationControllerUpdater.hpp
@@ -7,6 +7,8 @@
 #include "Zenject/IInitializable.hpp"
 #include "GlobalNamespace/LevelFilteringNavigationController.hpp"
 #include "GlobalNamespace/LevelCollectionViewController.hpp"
+#include "GlobalNamespace/LevelCollectionNavigationController.hpp"
+#include "GlobalNamespace/BeatmapLevelsModel.hpp"
 #include "SongLoader/RuntimeSongLoader.hpp"
 #include <vector>
 
@@ -14,11 +16,13 @@ DECLARE_CLASS_CODEGEN_INTERFACES(SongCore::SongLoader, NavigationControllerUpdat
     DECLARE_OVERRIDE_METHOD_MATCH(void, Initialize, &Zenject::IInitializable::Initialize);
     DECLARE_OVERRIDE_METHOD_MATCH(void, Dispose, &System::IDisposable::Dispose);
 
+    DECLARE_INSTANCE_FIELD_PRIVATE(GlobalNamespace::BeatmapLevelsModel*, _beatmapLevelsModel);
     DECLARE_INSTANCE_FIELD_PRIVATE(RuntimeSongLoader*, _runtimeSongLoader);
     DECLARE_INSTANCE_FIELD_PRIVATE(GlobalNamespace::LevelFilteringNavigationController*, _levelFilteringNavigationController);
+    DECLARE_INSTANCE_FIELD_PRIVATE(GlobalNamespace::LevelCollectionNavigationController*, _levelCollectionNavigationController);
     DECLARE_INSTANCE_FIELD_PRIVATE(GlobalNamespace::LevelCollectionViewController*, _levelCollectionViewController);
 
-    DECLARE_CTOR(ctor, RuntimeSongLoader* runtimeSongLoader, GlobalNamespace::LevelFilteringNavigationController* levelFilteringNavigationController, GlobalNamespace::LevelCollectionViewController* levelCollectionViewController);
+    DECLARE_CTOR(ctor, GlobalNamespace::BeatmapLevelsModel* beatmapLevelsModel, RuntimeSongLoader* runtimeSongLoader, GlobalNamespace::LevelFilteringNavigationController* levelFilteringNavigationController, GlobalNamespace::LevelCollectionNavigationController* levelCollectionNavigationViewController, GlobalNamespace::LevelCollectionViewController* levelCollectionViewController);
 
     private:
         void CustomLevelPacksRefreshed(CustomBeatmapLevelsRepository* levelsRepository);
--- src/SongLoader/NavigationControllerUpdater.cpp
+++ src/SongLoader/NavigationControllerUpdater.cpp
@@ -2,16 +2,21 @@
 
 #include "GlobalNamespace/LevelCollectionTableView.hpp"
 #include "SongLoader/CustomBeatmapLevelsRepository.hpp"
+#include "System/Collections/Generic/IReadOnlyList_1.hpp"
 #include "UnityEngine/GameObject.hpp"
+#include "HMUI/TableView.hpp"
+#include "HMUI/ScrollView.hpp"
 #include "logging.hpp"
 
 DEFINE_TYPE(SongCore::SongLoader, NavigationControllerUpdater);
 
 namespace SongCore::SongLoader {
-    void NavigationControllerUpdater::ctor(RuntimeSongLoader* runtimeSongLoader, GlobalNamespace::LevelFilteringNavigationController* levelFilteringNavigationController, GlobalNamespace::LevelCollectionViewController* levelCollectionViewController) {
+    void NavigationControllerUpdater::ctor(GlobalNamespace::BeatmapLevelsModel* beatmapLevelsModel, RuntimeSongLoader* runtimeSongLoader, GlobalNamespace::LevelFilteringNavigationController* levelFilteringNavigationController, GlobalNamespace::LevelCollectionNavigationController* levelCollectionNavigationController, GlobalNamespace::LevelCollectionViewController* levelCollectionViewController) {
         _runtimeSongLoader = runtimeSongLoader;
         _levelFilteringNavigationController = levelFilteringNavigationController;
+        _levelCollectionNavigationController = levelCollectionNavigationController;
         _levelCollectionViewController = levelCollectionViewController;
+        _beatmapLevelsModel = beatmapLevelsModel;
     }
 
     void NavigationControllerUpdater::Initialize() {
@@ -28,12 +33,34 @@ namespace SongCore::SongLoader {
         // if this is null, the controller wasn't setup yet, just make sure the packs are setup
         if (!_levelFilteringNavigationController->_allBeatmapLevelPacks) _levelFilteringNavigationController->SetupBeatmapLevelPacks();
 
-        // get the selected level to keep the selection after the songs update
-        auto tableView = _levelCollectionViewController->_levelCollectionTableView;
-        auto selectedLevel = tableView->_selectedBeatmapLevel;
-        _levelFilteringNavigationController->UpdateCustomSongs();
-        if (selectedLevel) { // if no level, don't keep selection
-            tableView->SelectLevel(selectedLevel);
+        GlobalNamespace::BeatmapLevelPack* packToSelect = nullptr;
+
+        // get and set pack info
+        auto pack = _levelFilteringNavigationController->selectedBeatmapLevelPack;
+        if (pack) {
+            // check whether the pack was still in the beatmap levels model
+            pack = _beatmapLevelsModel->GetLevelPack(pack->packID);
+            if (packToSelect) {
+                _levelFilteringNavigationController->_levelPackIdToBeSelectedAfterPresent = pack->packID;
+            }
         }
+
+        // get the selected level to keep the selection after the songs update,
+        auto levelCollectionTableView = _levelCollectionViewController->_levelCollectionTableView;
+        auto selectedLevel = levelCollectionTableView ? levelCollectionTableView->_selectedBeatmapLevel : nullptr;
+
+        // get and set level info
+        if (packToSelect && selectedLevel) { // check whether it's still in the beatmap levels model
+            // check whether the level was still in the beatmap levels model
+            _levelCollectionNavigationController->_beatmapLevelToBeSelectedAfterPresent = _beatmapLevelsModel->GetBeatmapLevel(selectedLevel->levelID);
+        }
+
+        auto tableView = levelCollectionTableView ? levelCollectionTableView->_tableView : nullptr;
+        auto scrollView = tableView ? tableView->_scrollView : nullptr;
+        auto pos = scrollView ? scrollView->position : 0;
+
+        _levelFilteringNavigationController->UpdateCustomSongs();
+
+        if (scrollView) scrollView->ScrollTo(pos, false);
     }
 }
